1e64464e8780e2b3cfdde7af0e9e309a51845a98
Vishal Shrivastava <vishal71shrivastava@gmail.com>
2025-06-17 11:07:25 +0530

    changes in debug and allowed hosts

--- image_admin/settings.py
+++ image_admin/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e38ml&7!(1z)r4h3!d0e5edmy54_phq)ax5g!nmx3z9470y-b&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Allow all hosts, adjust as necessary for production
 
 
 # Application definition
