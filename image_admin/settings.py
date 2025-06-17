96b2bfd4fa2e7ae5c533351352ef49634564954c
Vishal Shrivastava <vishal71shrivastava@gmail.com>
2025-06-17 11:03:30 +0530

    changes in setting file

--- image_admin/settings.py
+++ image_admin/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'image_admin.urls'
 
@@ -138,7 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- imageapp/urls.py
+++ imageapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import upload_image
+from django.conf import settings
+from django.conf.urls.static import static 
+import os
 
 urlpatterns = [
     path('', upload_image, name='upload_image'),
 ]
+if settings.DEBUG or 'RENDER' in os.environ:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
